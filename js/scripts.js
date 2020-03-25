97451c754831e897f1fdb50c95c7a44c3dd54aea
mai cook <mailiecook@yahoo.com>
2020-03-25 09:33:05 -0700

    add form, input and label tags in index.html

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("form0ne").submit(function(event) {
+    var adjective1Input = $("input#adjective1").val();
+    var animalInput = $("input#animal").val
+    var adjective2Input = $("input#adjective2").val();
+    var nameInput = $("input#name").val();
+    var adjective3Input = $("input#adjective3").val();
+    var adjective4Input = $("input#adjective4").val();
+    var adjective5Input = $("input#adjective5").val();
+    var adjective6Input = $("input#adjective6").val();
+    var verb1Input = $("input#verb1").val();
+    var verb2Input = $("input#verb2").val();
+    var nounInput = $("input#noun").val();
+    var adjective2Input = $("input#adjective2").val();
+  })
+})
