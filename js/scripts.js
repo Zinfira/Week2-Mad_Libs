c8185450e3a923b51c0e9929f04c7d706518eb07
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-03-30 09:11:56 -0700

    Use arrays and looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
   $("#form0ne").submit(function(event) {
     event.preventDefault();
-    var adjective1Input = $("input#adjective1").val();
-    var animalInput = $("input#animal").val();
-    var adjective2Input = $("input#adjective2").val();
-    var nameInput = $("input#name").val();
-    var adjective3Input = $("input#adjective3").val();
-    var adjective4Input = $("input#adjective4").val();
-    var adjective5Input = $("input#adjective5").val();
-    var adjective6Input = $("input#adjective6").val();
-    var verb1Input = $("input#verb1").val();
-    var verb2Input = $("input#verb2").val();
-    var nounInput = $("input#noun").val();
-    var verb3Input = $("input#verb3").val();
-    var verb4Input = $("input#verb4").val();
-    var personInput = $("input#person").val();
+    var blanks = ["adjective1", "animal", "adjective2", "name", "adjective3", "adjective4", "adjective5", "adjective6", "verb1", "verb2", "noun", "verb3", "verb4", "person"]
+    //var adjective1Input = $("input#adjective1").val();
+    //var animalInput = $("input#animal").val();
+    //var adjective2Input = $("input#adjective2").val();
+    //var nameInput = $("input#name").val();
+    //var adjective3Input = $("input#adjective3").val();
+    //var adjective4Input = $("input#adjective4").val();
+    //var adjective5Input = $("input#adjective5").val();
+    //var adjective6Input = $("input#adjective6").val();
+    //var verb1Input = $("input#verb1").val();
+    //var verb2Input = $("input#verb2").val();
+    //var nounInput = $("input#noun").val();
+    //var verb3Input = $("input#verb3").val();
+    //var verb4Input = $("input#verb4").val();
+    //var personInput = $("input#person").val();
 
-    console.log(adjective1Input, animalInput, adjective2Input, nameInput);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    })
+    
 
+    /*
     $(".adjective1").text(adjective1Input); 
     $(".animal").text(animalInput);
     $(".adjective2").text(adjective2Input);
@@ -32,6 +38,7 @@ $(document).ready(function() {
     $(".verb3").text(verb3Input);
     $(".verb4").text(verb4Input);
     $(".person").text(personInput);
+    */
 
     $("#story").show();
 
